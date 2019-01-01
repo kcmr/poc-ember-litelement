900036bb22c96e3edcb38ef0176cfdb379006e7e
Kus Cámara <kcmr@users.noreply.github.com>
2019-01-01 12:30:52 +0100

    use a computed property for the avatar

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,16 +1,29 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 import '@kuscamara/sample-lit-element'
 
 export default Controller.extend({
   vip: true,
+
   name: 'Firstname Lastname',
+
   email: 'sample@email.com',
-  avatar: 'https://api.adorable.io/avatars/120/random-0.png',
+
   phone: '666 666 666',
+
   phoneVisibilityDuration: 1000,
+
   cats: [
     'Miau',
     'Meow',
     'Meee'
-  ]
+  ],
+
+  avatarSrc: 'https://api.adorable.io/avatars/',
+
+  avatarSize: 100,
+
+  avatar: computed('avatarSrc', 'avatarSize', function() {
+    return `${this.avatarSrc}${this.avatarSize}/random-0.png`;
+  })
 });
