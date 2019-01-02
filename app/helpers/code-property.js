2763c00c52510704151324303d1941a42d17bd2d
Kus Cámara <kcmr@users.noreply.github.com>
2019-01-02 22:54:42 +0100

    refactor code-property helper

--- app/helpers/code-property.js
+++ app/helpers/code-property.js
@@ -1,9 +1,8 @@
 import { helper } from '@ember/component/helper';
 import { htmlSafe } from '@ember/string';
 
-export function codeProperty(params/*, hash*/) {
-  const [name] = params;
-  return htmlSafe(name ? `<code>{{${name}}}</code>` : '');
+export function codeProperty([value]/*, hash*/) {
+  return htmlSafe(value ? `<code>{{${value}}</code>` : '');
 }
 
 export default helper(codeProperty);
