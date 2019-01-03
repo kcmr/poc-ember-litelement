0bc0fbd31b969db5b375600131fa16ed2ec76308
Kus Cámara <kcmr@users.noreply.github.com>
2019-01-03 01:57:01 +0100

    add missing bracket in code-property helper

--- app/helpers/code-property.js
+++ app/helpers/code-property.js
@@ -2,7 +2,7 @@ import { helper } from '@ember/component/helper';
 import { htmlSafe } from '@ember/string';
 
 export function codeProperty([value]/*, hash*/) {
-  return htmlSafe(value ? `<code>{{${value}}</code>` : '');
+  return htmlSafe(value ? `<code>{{${value}}}</code>` : '');
 }
 
 export default helper(codeProperty);
