6598d269d17ad16123911454bbf93a1c038205bd
Kus Cámara <kcmr@users.noreply.github.com>
2019-01-04 19:47:08 +0100

    not required to import the template in a component

--- app/components/form-property-field/component.js
+++ app/components/form-property-field/component.js
@@ -1,10 +1,7 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import template from './template';
 
 export default Component.extend({
-  layout: template,
-
   classNames: ['field'],
 
   type: 'text',
